cda32f47029d9124d823cf2975284d61d2ba6944
Sunny <faronwmoore@gmail.com>
2023-01-26 14:47:09 -0600

    Background job

---
+++ ElectricCarService/Jobs/MyBackgroundJob.cs
@@ -0,0 +1,21 @@
+﻿namespace ElectricCarService.Jobs;
+
+public class MyBackgroundJob: BackgroundService
+{
+    private readonly ILogger<MyBackgroundJob> _logger;
+
+    public MyBackgroundJob(ILogger<MyBackgroundJob> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Hi");
+            await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+        }
+        _logger.LogInformation("Bye");
+    }
+}
--- ElectricCarService/Program.cs
+++ ElectricCarService/Program.cs
@@ -1,7 +1,9 @@
+using ElectricCarService.Jobs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHostedService<MyBackgroundJob>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
